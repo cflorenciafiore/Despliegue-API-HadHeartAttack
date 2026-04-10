3fe4263d522d9c763cbe3c5770f817dce12052cc
Alejandro <arbide.ale@gmail.com>
2026-04-10 22:57:39 +0200

    model_final

--- model.py
+++ model.py
@@ -17,13 +17,48 @@ columns = [
     "AgeCategory","WeightInKilograms","CovidPos","ChestScan","PneumoVaxEver"
 ]
 
+defaults = {
+    "State": "California",
+    "Sex": "Male",
+    "GeneralHealth": "Good",
+    "PhysicalHealthDays": 0,
+    "MentalHealthDays": 0,
+    "LastCheckupTime": "Within past year",
+    "PhysicalActivities": "Yes",
+    "SleepHours": 7,
+    "RemovedTeeth": "None",
+    "HadAngina": "No",
+    "HadStroke": "No",
+    "HadCOPD": "No",
+    "HadDiabetes": "No",
+    "HadKidneyDisease": "No",
+    "HadArthritis": "No",
+    "HadSkinCancer": "No",
+    "AlcoholDrinkers": "Yes",
+    "SmokerStatus": "Never smoked",
+    "DifficultyWalking": "No",
+    "DifficultyDressingBathing": "No",
+    "DifficultyErrands": "No",
+    "DeafOrHardOfHearing": "No",
+    "BlindOrVisionDifficulty": "No",
+    "DifficultyConcentrating": "No",
+    "AgeCategory": "Age 60-64",
+    "WeightInKilograms": 80,
+    "CovidPos": "No",
+    "ChestScan": "No",
+    "PneumoVaxEver": "Yes"
+}
+
 def predict(features: dict):
     global model
 
     if model is None:
         model = joblib.load(model_path)
 
-    df = pd.DataFrame([features])
+    # Rellenar campos faltantes con defaults
+    complete_features = {**defaults, **features}
+
+    df = pd.DataFrame([complete_features])
     df = df[columns]
 
     prediction = model.predict(df)
